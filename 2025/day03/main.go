c7dcb84767b172046990c60e34fe271a93f78420
Muhammad <usamaqaisranifps@gmail.com>
2025-12-03 16:03:17 +0000

    solved day03 part02

--- 2025/day03/main.go
+++ 2025/day03/main.go
@@ -21,6 +21,13 @@ func main() {
 		return
 	}
 	fmt.Println("PartOne:", partOneSol)
+
+	partTwoSol, err := partTwo(refinedInput)
+	if err != nil {
+		fmt.Println("Error in PartTwo:", err)
+		return
+	}
+	fmt.Println("PartTwo:", partTwoSol)
 }
 
 func partOne(input []string) (int, error) {
@@ -46,4 +53,60 @@ func partOne(input []string) (int, error) {
 		totalMax += currMax
 	}
 	return totalMax, nil
+}
+
+func partTwo(input []string) (int, error) {
+	totalJolts := 0
+	for _, bank := range input {
+		requiredNums := 12
+		var stack []int
+		for index, numStr := range bank {
+			remainingNums := len(bank) - index 
+			num, err := strconv.Atoi(string(numStr))
+			if err != nil {
+				return -1, err
+			}
+			//If remaining numbers less than reqruired, just keep appending.
+			if remainingNums <= requiredNums {
+				stack = append(stack, num)
+				requiredNums--
+				continue
+			}
+			// If stack is empty append the number
+			if len(stack) < 1 {
+				stack = append(stack, num)
+				requiredNums--
+				continue
+			}
+			
+			//Pop until a bigger number is found
+			for len(stack) > 0 && num > stack[len(stack)-1] { 
+				if len(stack)+remainingNums > 12 { 
+					stack = stack[:len(stack)-1]
+					requiredNums++
+				} else {
+					break 
+				}
+			}
+
+			if len(stack) < 12 {
+				stack = append(stack, num)
+				requiredNums--
+			}
+		}
+		joltStr := ""
+		for _, num := range stack {
+			joltStr += fmt.Sprintf("%d", num)
+		}
+		fmt.Println(joltStr)
+		if joltStr == "" {
+			continue
+		}
+		currJolts, err := strconv.Atoi(joltStr)
+		if err != nil {
+			return -1, err
+		}
+		totalJolts += currJolts
+	}
+	return totalJolts, nil
 }
