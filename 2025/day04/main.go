43cf38d38f98b61375855402d2cdb6f6d5525fb5
Muhammad <usamaqaisranifps@gmail.com>
2025-12-04 14:11:07 +0000

    solved day04 part01

--- 2025/day04/main.go
+++ 2025/day04/main.go
@@ -23,20 +23,26 @@ func main() {
 		currRow := []rune(row)
 		refinedInput = append(refinedInput,currRow)
 	}
+	partOneSol, err := partOne(refinedInput)
+	if err != nil {
+		fmt.Println("Error occured in PartOne:", err)
+		return
+	}
+	fmt.Println("PartOne:",partOneSol)
 }
 
 func partOne(input [][]rune) (int, error) {
-	totalRow := input[0]
-	totalCols := input[0][0]
-	var visited map[string]bool
+	count := 0
 	for i, row := range input {
-		for j, col := range row {
-			if string(input[i][j]) == "@" {
-				bfs(i, j)
+		for j, _ := range row {
+			currCount := expand(i,j, input, 1)
+			fmt.Println("Count:", currCount)
+			if currCount < 4 && string(input[i][j]) != "." {
+				count += 1
 			}
 		}
 	}
-	return 0, nil
+	return count, nil
 }
 
 func validPosition(row, col, totalRows, totalCols int) bool {
@@ -45,7 +51,30 @@ func validPosition(row, col, totalRows, totalCols int) bool {
 	return validRow && validCols
 }
 
-func bfs(r, c int) {
-	directions := [][]int{ { 0,1 },{ 0,-1 },{ 1,1 },{ 1,-1 },{ -1,1 },{ -1,-1 },{ 1,0 },{ -1,0 } }
-	fmt.Println(directions)
+func expand(r, c int, grid [][]rune, steps int) int {
+//	directions := [][]int{{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1},{1,0},{-1,0}}
+	totalRows := len(grid)
+	totalCols := len(grid[0])
+
+	//base cases
+	if !validPosition(r, c, totalRows, totalCols) {
+		return 0
+	}
+	if string(grid[r][c]) != "@" {
+		return 0
+	}
+	if steps == 0 {
+		return 1
+	}
+	nextStep := steps - 1
+	n1 := expand(r+1,c, grid, nextStep)
+	n2 := expand(r,c+1, grid, nextStep)
+	n3 := expand(r-1,c, grid, nextStep)
+	n4 := expand(r,c-1, grid, nextStep)
+	n5 := expand(r+1,c+1, grid, nextStep)
+	n6 := expand(r-1,c-1, grid, nextStep)
+	n7 := expand(r+1,c-1, grid, nextStep)
+	n8 := expand(r-1,c+1, grid, nextStep)
+
+	return n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8
 }
