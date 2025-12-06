8a81a7009cf551edeeb3cb0f600106ac12c10ef4
Muhammad <usamaqaisranifps@gmail.com>
2025-12-06 21:15:34 +0000

    solved day06 part02

--- 2025/day06/main.go
+++ 2025/day06/main.go
@@ -16,14 +16,26 @@ func main() {
 		os.Exit(1)
 	}
 	strSplit := strings.Split(strings.TrimRight(string(fileContent), "\n"), "\n")
-	operations := strings.Split(strings.ReplaceAll(strSplit[len(strSplit)-1], " ", ""), "")
+
 	numbers := strSplit[:len(strSplit)-1]
-	partOneSol, err := partOne(numbers, operations)
+	rawLastLine := strSplit[len(strSplit)-1]
+
+	operationsPart1 := strings.Split(strings.ReplaceAll(rawLastLine, " ", ""), "")
+
+	partOneSol, err := partOne(numbers, operationsPart1)
 	if err != nil {
 		log.Fatal("Error in part one:", err)
 		os.Exit(1)
 	}
 	fmt.Println("PartOne:", partOneSol)
+
+	operationsPart2 := []string{rawLastLine}
+	partTwoSol, err := partTwo(numbers, operationsPart2)
+	if err != nil {
+		log.Fatal("Error in part two:", err)
+		os.Exit(1)
+	}
+	fmt.Println("PartTwo:", partTwoSol)
 }
 
 func partOne(numbers []string, operations []string) (int, error) {
@@ -40,17 +52,94 @@ func partOne(numbers []string, operations []string) (int, error) {
 				continue
 			}
 
-			if operations[j] == "+" {
-				answersList[j] += currNum
-			} else {
-				answersList[j] *= currNum
+			if j < len(operations) {
+				if operations[j] == "+" {
+					answersList[j] += currNum
+				} else {
+					answersList[j] *= currNum
+				}
 			}
 		}
 	}
-	fmt.Println(answersList)
 	answer := 0
 	for _, num := range answersList {
 		answer += num
 	}
 	return answer, nil
+}
+
+func partTwo(numbers []string, operations []string) (int, error) {
+	opLine := operations[0]
+	var opPos []int
+	for i, c := range opLine {
+		if c == '+' || c == '*' {
+			opPos = append(opPos, i)
+		}
+	}
+	result := int64(0)
+	for i := range opPos {
+		start := opPos[i]
+		end := len(opLine)
+		if i+1 < len(opPos) {
+			end = opPos[i+1]
+		}
+		var colText []string
+		for _, line := range numbers {
+			if start < len(line) {
+				currEnd := end
+				if currEnd > len(line) {
+					currEnd = len(line)
+				}
+				colText = append(colText, line[start:currEnd])
+			} else {
+				colText = append(colText, "")
+			}
+		}
+
+		nums := extractVertical(colText)
+		op := rune(opLine[start])
+		result += applyOp(nums, op)
+	}
+
+	return int(result), nil
+}
+
+func extractVertical(lines []string) []int {
+	maxH := len(lines)
+	var out []int
+
+	if len(lines) == 0 {
+		return out
+	}
+
+	width := len(lines[0])
+
+	for col := 0; col < width; col++ {
+		var b strings.Builder
+		for row := 0; row < maxH; row++ {
+			if col < len(lines[row]) && lines[row][col] != ' ' {
+				b.WriteByte(lines[row][col])
+			}
+		}
+		if b.Len() > 0 {
+			n, _ := strconv.Atoi(b.String())
+			out = append(out, n)
+		}
+	}
+	return out
+}
+
+func applyOp(nums []int, op rune) int64 {
+	if len(nums) == 0 {
+		return 0
+	}
+	acc := int64(nums[0])
+	for _, n := range nums[1:] {
+		if op == '+' {
+			acc += int64(n)
+		} else {
+			acc *= int64(n)
+		}
+	}
+	return acc
 }
